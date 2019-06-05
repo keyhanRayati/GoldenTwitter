8823c9dc0e397855cc321ac694e145ded68bf8a9
keyhan <keyhan@gmail.com>
2019-06-05 16:58:29 +0430

    search complete

--- app/src/main/java/ir/co/arca/twitter/HomeFragment.java
+++ app/src/main/java/ir/co/arca/twitter/HomeFragment.java
@@ -43,6 +43,22 @@ public class HomeFragment extends Fragment {
     }
 
     private void fillMessageList() {
+        twitteList.add(new Twitte(R.drawable.default_avatar, "Keyhan", "@_say10__",
+                "32m", "It is a page when looking at its it has a more-or-less normal distribution of letters",
+                R.drawable.ic_mention, R.drawable.ic_renew,
+                R.drawable.ic_favorite, "4", "4",
+                "4"));
+        twitteList.add(new Twitte(R.drawable.default_avatar, "Keyhan", "@_say10__",
+                "32m", "It is a long estd\n" +
+                " by the readable\n" +
+                " content of a page \n" +
+                "when looki The point of \n" +
+                "using Ipsum is\n " +
+                "that it has a more-or-less nl di\n" +
+                "stributiof letters",
+                R.drawable.ic_mention, R.drawable.ic_renew,
+                R.drawable.ic_favorite, "4", "4",
+                "4"));
         for (int i = 0; i < 50; i++) {
             twitteList.add(new Twitte(R.drawable.default_avatar, "Keyhan", "@_say10__",
                     "32m", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters",
